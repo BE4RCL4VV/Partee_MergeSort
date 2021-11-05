e8497722b640ed1d9b9084a7a688d0531cfc1581
Mitch Partee <mitch.partee@student.centralia.edu>
2021-11-04 23:54:19 -0700

    Add project files.

---
+++ MergeSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partee_MergeSort
+{
+    class MergeSort
+    {
+        static void Merge(int[] arr, int left, int middle, int right)
+        {
+        // Create the single array, preserve length index, copy left index for temp array use
+            int totalLength = (right - left + 1);
+            int[] tempArray = new int[arr.Length];
+            int leftIndex = left;
+        // Separate left and right "sub arrays" with a middle index point
+            int middleZero = (middle - 1);
+
+            while (left <= middleZero && middle <= right)
+            {
+            // Compare values
+                if (arr[left] >= arr[middle])
+                {
+                    tempArray[leftIndex++] = arr[left++];
+                }
+                else
+                {
+                    tempArray[leftIndex++] = arr[middle++];
+                }
+            }
+            while (left <= middleZero)
+            {
+                tempArray[leftIndex++] = arr[left++];
+            }
+            while (middle <= right)
+            {
+                tempArray[leftIndex++] = arr[middle++];
+            }
+        // since right is the only preserved index left, I used it to copy the array
+            for (int i = 0; i < totalLength; i++)
+            {
+                arr[right] = tempArray[right];
+                right--;
+            }
+        }
+        public static void Sort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (right + left) / 2;
+            // recursive call to Sort() Method
+                Sort(arr, left, middle);
+                Sort(arr, middle + 1, right);
+            // Merge part of the merge sort
+                Merge(arr, left, middle + 1, right);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Partee_MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 30, 900, 55, 54, 56, 71, -43, 0, 4529, 555, 639, 101, 102, 100, 5, 82, 50, -77, 14, 99, 104, 1 };
+
+            MergeSort.Sort(array, 0, array.Length - 1);
+            PrintArray(array);
+        }
+        static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
